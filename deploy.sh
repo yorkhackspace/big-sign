4b983f4962007c518babcc2db6f0b1248121b1e6
soweliniko <nico@itwont.work>
2024-04-10 22:30:23 +0100

    build before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cargo build
+cd frontend
+npm run build
+cd ..
 ssh yhs@big-sign.local "sudo systemctl stop big-sign"
 scp target/aarch64-unknown-linux-gnu/debug/yhs-sign yhs@big-sign.local:~
 scp -r static yhs@big-sign.local:~
