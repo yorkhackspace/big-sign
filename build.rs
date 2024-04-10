fd76c5039361ab0c82d06a8d4cb131bad451b6b9
soweliniko <nico@itwont.work>
2024-04-10 22:10:44 +0100

    update  buildscript

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
 
 fn main() {
-	let _ = Command::new("npx").args(&["vite", "build"]).current_dir("frontend/").status().unwrap();
+	let _ = Command::new("npm").args(&["run", "build"]).current_dir("frontend/").status().unwrap();
 }
